a4ce5a1a8d827f06135d818a42baa51fed4bdbba
Ayush Das <81952944+AshleyCodes-1@users.noreply.github.com>
2025-03-25 07:43:30 +0530

    updates to files

--- homeScript.js
+++ homeScript.js
@@ -23,6 +23,35 @@ function w3_close() {
     mySidebar.style.display = "none";
 }
 
+// Toggle Dropdown in Sidebar
+function toggleDropdownAppliances() {
+  var x = document.getElementById("sidebarDropdown-appliances");
+  if (x.classList.contains("w3-show")) {
+    x.classList.remove("w3-show");
+  } else {
+    x.classList.add("w3-show");
+  }
+}
+
+function toggleDropdownHome(){
+  var y = document.getElementById("sidebarDropdown-home");
+  if (y.classList.contains("w3-show")) {
+    y.classList.remove("w3-show");
+  } else {
+    y.classList.add("w3-show");
+  }
+}
+
+function toggleDropdownVehicle(){
+  var z = document.getElementById("sidebarDropdown-vehicle");
+  if (z.classList.contains("w3-show")) {
+    z.classList.remove("w3-show");
+  } else {
+    z.classList.add("w3-show");
+  }
+}
+
+
 // What are you looking for section
 var slideIndex = 0;
 var slides = document.querySelectorAll('.service-slide');
@@ -34,3 +63,31 @@ function plusDivs(n) {
 }
 
 plusDivs(0);
+
+// Image slider header
+var slideImageIndex = 1;
+showImageDivs(slideImageIndex);
+
+function plusImageDivs(n) {
+  showImageDivs(slideImageIndex += n);
+}
+
+function currentDiv(n) {
+  showImageDivs(slideImageIndex = n);
+}
+
+function showImageDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideImageIndex = 1}    
+  if (n < 1) {slideImageIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-red", "");
+  }
+  x[slideImageIndex-1].style.display = "block";  
+  dots[slideImageIndex-1].className += " w3-red";
+}
